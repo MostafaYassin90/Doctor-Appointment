e75ebf4392eaa4714b5939c6be3d4670618825ae
Mostafa Yassin <mostafayassin292@gmail.com>
2025-07-24 16:51:57 +0300

    add changes

--- backend/config/mongoDB.js
+++ backend/config/mongoDB.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const conntectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGODB_URL}/doctor-appointment`);
+    await mongoose.connect(`${process.env.MONGODB_URL}`, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log("DB Connection Is Successfully!");
   } catch (error) {
     console.log("Failed To Connect DB!");
